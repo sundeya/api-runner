af9bbe9142a49f21eb7a1ee1e7348380c47b4d35
sundeya <245681358@qq.com>
2019-09-13 18:43:17 +0800

    Crosstable更新

--- src/main/java/com/lemon/api/api/pojo/CrossTable.java
+++ src/main/java/com/lemon/api/api/pojo/CrossTable.java
@@ -1,11 +1,10 @@
 package com.lemon.api.api.pojo;
 
-import lombok.Data;
-
-@Data
 public class CrossTable {
-    private String first;
-    private String second;
-    private String apiId;
-    private String refer;
+
+        private String first;
+        private String second;
+        private String apiId;
+        private String refer;
+
 }
